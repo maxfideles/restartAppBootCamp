8e34d9c3f49afe7538d408a6069fdf12735ee40a
maxfideles <maxfideles24@gmail.com>
2023-05-11 16:14:53 -0300

    Initial Commit

---
+++ restartAppBootCamp/restartAppBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  restartAppBootCampApp.swift
+//  restartAppBootCamp
+//
+//  Created by Max Victor on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct restartAppBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
