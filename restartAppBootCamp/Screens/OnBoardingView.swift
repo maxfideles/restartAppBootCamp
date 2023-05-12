1a02e8ec481da2deabfaf7673e6089a34a8f4590
maxfideles <maxfideles24@gmail.com>
2023-05-11 22:42:13 -0300

    UI OnBoarding Screen

--- restartAppBootCamp/Screens/OnBoardingView.swift
+++ restartAppBootCamp/Screens/OnBoardingView.swift
@@ -14,18 +14,100 @@ struct OnBoardingView: View {
     
     var body: some View {
       
-        VStack{
-            Text("OnBoarding")
-                .font(.largeTitle)
-            
-            Button {
-                isOnBoardingViewActive.toggle()
-            } label: {
-                Text("Start")
-            }
-
-            
-        }
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all,edges: .all)
+            VStack(spacing: 20){
+              //MARK: - Header
+                Spacer()
+                
+                VStack(spacing: 0){
+                    Text("Share")
+                        .font(.system(size: 60))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    Text("""
+                    It's not how much we give but
+                    how much lovewe putinto giving.
+                    """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal,10)
+                }//: Header
+                
+            //MARK: - Center
+                ZStack{
+                    ZStack{
+                        Circle()
+                            .stroke(.white.opacity(0.2),lineWidth: 40)
+                            .frame(width: 260,height: 260,alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2),lineWidth: 80)
+                            .frame(width: 260,height: 260,alignment: .center)
+                        
+                    }//:ZStack
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                    
+                }//: Center
+                
+                Spacer()
+                
+             //MARK: - Footer
+                
+                ZStack{
+                    //Parts of the custom Buttom
+                    
+                    //1. Background (Static)
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    //2. Call-to-Action (Static)
+                    Text("Get Started")
+                        .font(.system(.title3,design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x:20)
+                    //3. Capsule (Dynamic width)
+                    HStack{
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width:80)
+                        Spacer()
+                    }
+                    //4. Circle (Draggable)
+                    HStack {
+                        ZStack{
+                            Capsule()
+                                .fill(Color("ColorRed"))
+                            Capsule()
+                                .fill(Color.black.opacity(0.2))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }//: ZStack
+                        .foregroundColor(.white)
+                    .frame(width:80,height: 80,alignment: .center)
+                    .onTapGesture {
+                        isOnBoardingViewActive = false
+                    }
+                        
+                        Spacer()
+                    }//:HStack
+                    
+                }//:Fotter
+                .frame(height:80,alignment: .center)
+                .padding()
+            }//: VSTACK
+        }//:ZSTACK
         
     }
 }
